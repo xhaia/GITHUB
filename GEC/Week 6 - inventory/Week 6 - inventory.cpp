2f2795dbb8cf2c28d462ece64646f0da0dcef3c9
xhaia <76160678+xhaia@users.noreply.github.com>
2021-11-15 14:50:41 +0000

    inventory

--- GEC/Week 6 - inventory/Week 6 - inventory.cpp
+++ GEC/Week 6 - inventory/Week 6 - inventory.cpp
@@ -5,18 +5,43 @@ using namespace std;
 
 int main()
 {
-    char staff;
+    for (;;)
+    {
+        char staff;
+        char dagger;
 
-    const int MAX_ITEMS = 4;
-    int numItems = 0;
-    string inventory[MAX_ITEMS];
-    inventory[numItems++] = "sword";
-    inventory[numItems++] = "battle axe";
-    inventory[numItems++] = "healing potion";
-    inventory[numItems++] = "dagger";
+        const int MAX_ITEMS = 4;
+        int numItems = 0;
+        string inventory[MAX_ITEMS];
+        inventory[numItems++] = "sword";
+        inventory[numItems++] = "battle axe";
+        inventory[numItems++] = "healing potion";
+        inventory[numItems++] = "dagger";
 
-    cout << "You have found a magic staff, would you like to take it? y or n" << endl;
-    cin >> staff;
+        cout << "You have found a wizard's staff, would you like to take it? y or n" << endl;
+        cin >> staff;
 
+        if (staff == 'y')
+        {
+            cout << "Would you like to replace the dagger with the staff? y or n" << endl;
+            cin >> dagger;
 
+            if (dagger == 'y')
+            {
+                inventory[3] = "wizard's staff";
+                cout << "You have replaced the dagger with the staff! " << endl;
+            }
+            else if (dagger == 'n')
+            {
+                cout << "You have left behind the wizard's staff! " << endl;
+            }
+        }
+        else if (staff == 'n')
+        {
+            cout << "You have left behind the wizard's staff! " << endl;
+        }
+
+        for (int i = 0; i < 4; i++)
+            cout << inventory[i] << endl;
+    }
 }
