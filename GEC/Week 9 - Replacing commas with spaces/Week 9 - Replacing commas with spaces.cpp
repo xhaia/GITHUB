86f1e0a21350ab2a4e56344838ad5b4a43c8961b
s014372k <s014372k@S500A-03>
2022-01-04 12:56:40 +0000

    h
    g

---
+++ GEC/Week 9 - Replacing commas with spaces/Week 9 - Replacing commas with spaces.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+void comma2blank(char* textToChange);
+
+int main()
+{
+    char* textToChange = new char;
+
+    cout << "Enter a sentence! (commas instead of spaces) " << endl;
+    cin.getline(textToChange, 100);
+
+    cout << "\nBefore change: " << endl;
+    cout << textToChange << endl;
+
+    comma2blank(textToChange);
+
+    cout << "\nAfter change: " << endl;
+    cout << comma2blank << endl;
+
+    return 0;
+}
+
+void comma2blank(char* textToChange)
+{
+    string ptextToChange;
+
+    cout << *textToChange << endl;
+    getline(cin, ptextToChange);
+
+    replace(ptextToChange.begin(), ptextToChange.end(), ',', ' ');
+
+    cout << ptextToChange << endl;
+    cin.getline(textToChange, 100);
+
+    textToChange;
+    return;
+}
